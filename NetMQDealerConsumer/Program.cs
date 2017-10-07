e47d79bce643b576845d18047f993aea9c8cc6ce
opedroso <opedroso@gmail.com>
2017-10-07 05:51:01 -0500

    Added Producer and Consumer

---
+++ NetMQDealerConsumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetMQDealerConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var end2 = new DealerSocket())
+            {
+                end2.Connect("tcp://localhost:5555");
+
+                var end2Task = Task.Run(() =>
+                {
+                    Console.WriteLine("ThreadId = {0}", Thread.CurrentThread.ManagedThreadId);
+                    var message = end2.ReceiveFrameString();
+                    Console.WriteLine(message);
+                });
+                Task.WaitAll(new[] { end2Task });
+            }
+            NetMQConfig.Cleanup();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NetMQDealerProducer/Program.cs
@@ -0,0 +1,30 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetMQDealerProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var end1 = new DealerSocket())
+            {
+                end1.Bind("tcp://*:5555");
+
+                var end1Task = Task.Run(() =>
+                {
+                    Console.WriteLine("ThreadId = {0}", Thread.CurrentThread.ManagedThreadId);
+                    Console.WriteLine("Sending hello down the inproc pipeline");
+                    end1.SendFrame("Hello");
+                });
+                Task.WaitAll(new[] { end1Task });
+                //Thread.Sleep(500);
+            }
+            NetMQConfig.Cleanup();
+            Console.ReadLine();
+        }
+    }
+}
